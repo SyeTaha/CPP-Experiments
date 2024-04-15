b492d4724d41b0c153818f06c5f18af78facb7b1
Syed Taha <syetaha@gmail.com>
2024-04-15 09:04:50 +0500

    Made Optimisations
    Optimized DrawTanPatt function and update animation logic.
    
    Renamed DrawTanPatt to AnimateTanPattern

--- src/Animations/AnimatingImages.cpp
+++ src/Animations/AnimatingImages.cpp
@@ -14,40 +14,45 @@ class Animator{
 
 public:
 
-
-    void DrawTanPatt(int a, int b, int r){
-        int frameNumber = 1920;
-        std::ofstream image;
-        int maxFrame = Height;
-
-        while(frameNumber <= maxFrame){       
-            int numOfLines = 0;
-            std::string frame =  "Rendered/" + std::to_string(frameNumber) + ".ppm";
-            image.open(frame, std::ios::trunc);
-            if(image.is_open()){
-                image << "P3" << std::endl;
-                image << Width << " " << Height << std::endl;
-                image << "255" << std::endl;   
-                for(int y = 0; y < frameNumber; y++){
-                    for(int x = 0; x < Width; x++){
-                        int Ox = x - a, Oy = y - b;
-                        if(tan(pow(Ox, 2) + pow(Oy, 2)) <= tan(pow(r, 2))) 
-                            image << "0 0 0" << std::endl;
-                        else 
-                            image << "0 40 40" << std::endl;
+    void AnimateTanPattern(int a, int b, int r) {
+        const std::string outputDir = "Rendered/";
+        const int maxFrame = 1920;
+        
+        for (int frameNumber = 1; frameNumber <= maxFrame; ++frameNumber) {
+            std::string frame = outputDir + std::to_string(frameNumber) + ".ppm";
+            std::ofstream image(frame, std::ios::trunc);
+            
+            if (image.is_open()) {
+                image << "P3\n";
+                image << Width << " " << Height << "\n";
+                image << "255\n";
+                
+                for (int y = 0; y < Height; ++y) {
+                    if (y <= frameNumber) {
+                        for (int x = 0; x < Width; ++x) {
+                            int Ox = x - a, Oy = y - b;
+                            if (tan(pow(Ox, 2) + pow(Oy, 2)) <= tan(pow(r, 2)))
+                                image << "0 0 0\n";
+                            else
+                                image << "0 150 150\n";
+                        }
+                    } else {
+                        // Fill the remaining lines with black
+                        for (int x = 0; x < Width; ++x) {
+                            image << "0 0 0\n";
+                        }
                     }
                 }
-                for(int y = frameNumber; y < Height; y++){
-                    for(int x = 0; x < Width; x++){
-                        image << "0 0 0" << std::endl;
-                    }
-                }
+                
+                std::cout << "\rRendered " << frameNumber << " frames" << std::flush;
+            } else {
+                std::cerr << "Error: Unable to open file " << frame << std::endl;
             }
-            std::cout << "Rendered " << frameNumber << " frames\n";
+            
             image.close();
-            frameNumber++;
         }
     }
+
 
 
 };
@@ -60,7 +65,7 @@ int main(){
 
     Animator m1;
 
-    m1.DrawTanPatt(Width/2, Height/2, 0);
+    m1.AnimateTanPattern(Width/2, Height/2, 0);
     //m1.render(0, 1000);
 
 
